99620cf5ff8b5a677f6b30915b2a8989028433eb
vrush2720 <99711352+vrush2720@users.noreply.github.com>
2022-11-17 17:38:44 +0530

    Update bank_app_db.sql

--- bank_app_db.sql
+++ bank_app_db.sql
@@ -1,3 +1,4 @@
 create database if not exists banking_db;
 use banking_db;
-create table if not exists banking_app_tb (Code varchar(10), Name varchar(10), Amount bigint default 0);
+create table if not exists banking_app_tb (Code varchar(10), Name varchar(10), Amount int);
+insert into banking_app_tb values ('a01','vrushali',0);
